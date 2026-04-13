5558e46f0e8aed34abfe1b5f7e7f821a7fa25ab0
12meklit <99590950+12meklit@users.noreply.github.com>
2026-04-13 17:30:33 +0300

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-    proxy: {
-      "/api": {
-        target: "http://localhost:5050",
-        changeOrigin: true,
-      },
-      "/uploads": {
-        target: "http://localhost:5050",
-        changeOrigin: true,
-      },
-    },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+import { defineConfig } from "vite"; 
+import react from "@vitejs/plugin-react-swc"; 
+import path from "path"; 
+ 
+// https://vitejs.dev/config/ 
+export default defineConfig({ 
+  server: { 
+    host: "::", 
+    port: 8080, 
+    hmr: { 
+      overlay: false, 
+    }, 
+    proxy: { 
+      "/api": { 
+        target: "http://localhost:5050", 
+        changeOrigin: true, 
+      }, 
+      "/uploads": { 
+        target: "http://localhost:5050", 
+        changeOrigin: true, 
+      }, 
+    }, 
+  }, 
+  plugins: [react()], 
+  resolve: { 
+    alias: { 
+      "@": path.resolve(__dirname, "./src"), 
+    }, 
+  }, 
+});
